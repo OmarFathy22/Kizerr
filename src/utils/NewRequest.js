6224fd33456bc435fab770e54e959aa9fc996309
Omar Fathy <91073589+OmarFathy22@users.noreply.github.com>
2023-08-05 10:15:54 +0300

    new

--- src/utils/NewRequest.js
+++ src/utils/NewRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
  const NewRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://kizerrpi.onrender.com',
+  // baseURL: 'http://localhost:3000',
   withCredentials: true
 })
 export default NewRequest
