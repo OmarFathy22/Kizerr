e4251d94c1242c93a60ee3f254ebaf77e9742137
Omar Fathy <91073589+OmarFathy22@users.noreply.github.com>
2023-12-21 05:15:07 +0200

    Update NewRequest.js

--- src/utils/NewRequest.js
+++ src/utils/NewRequest.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
  const NewRequest = axios.create({
   // baseURL: 'https://kizerrpi.onrender.com',
-  baseURL:'kizerr-api-production.up.railway.app',
+  baseURL:'https://kizerr-api-production.up.railway.app',
   // baseURL: 'http://localhost:3000', 
   withCredentials: true
 })
